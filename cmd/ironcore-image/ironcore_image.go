c207b1d4b30ab3ca2f0dc7a8cdb964cefe8639db
Maximilian Moehl <maximilian@moehl.eu>
2026-04-20 02:51:56 -0500

    Move main package to cmd/ironcore-image
    The main package at `cmd/` causes installation via `go install` to
    produce a binary called `cmd`. This commit moves the main package to
    `cmd/ironcore-image` to have `go install` produce a binary called
    `ironcore-image` as expected.
    
    This is a breaking change.
    
    Signed-off-by: Maximilian Moehl <maximilian@moehl.eu>

--- cmd/ironcore-image/ironcore_image.go
+++ cmd/ironcore-image/ironcore_image.go
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and IronCore contributors
 // SPDX-License-Identifier: Apache-2.0
 
-package ironcoreimage
+package main
 
 import (
 	"github.com/ironcore-dev/ironcore-image/cmd/build"
